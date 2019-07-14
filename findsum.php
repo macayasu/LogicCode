9d44c5de483be2adccc0190c5617765b3a044d12
Mine <mariuzcalvin@gmail.com>
2019-07-14 10:52:32 +0700

    Update FindSum

---
+++ findsum.php
@@ -0,0 +1,25 @@
+<?php
+
+function tes($number) {
+	
+$array = [1,5,6,8,7,2,4];
+$countarr = count($array);
+
+for ($i = 0; $i < $countarr; $i++)
+     {
+       for ($j = $i+1; $j < $countarr; $j++)
+           
+	   {
+		 if($array[$i]+$array[$j] == $number)
+		   {
+			   echo $array[$i].'+'.$array[$j].' = '.$number.' ';
+		   }
+				
+				
+        }
+	
+      }
+	
+}
+
+tes(7);

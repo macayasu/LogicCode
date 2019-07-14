6d7f779c200a5fc47bae3ae4af9f13a576a30000
Mine <mariuzcalvin@gmail.com>
2019-07-14 10:53:33 +0700

    Update findsum

--- findsum.php
+++ findsum.php
@@ -1,25 +1,20 @@
 <?php
 
-function tes($number) {
+function findsum($number) {
 	
 $array = [1,5,6,8,7,2,4];
 $countarr = count($array);
 
 for ($i = 0; $i < $countarr; $i++)
      {
-       for ($j = $i+1; $j < $countarr; $j++)
-           
+       for ($j = $i+1; $j < $countarr; $j++) 
 	   {
 		 if($array[$i]+$array[$j] == $number)
 		   {
 			   echo $array[$i].'+'.$array[$j].' = '.$number.' ';
-		   }
-				
-				
+		   }		
         }
-	
       }
-	
 }
 
-tes(7);
+findsum(7);
